ea5fcac15f6a5d0a5e5f8227400238b42b746ba7
ushapatturaj <ushadevipatturaj@gmail.com>
2020-04-30 16:33:47 +0200

    Learn Spark - Spark SQL - creating temp view, listing temp view using catalog and sparksql,selecting specific columns and rows using spark sql

--- src/main/scala/com/allaboutspark/chapter/one/tutorial_2/Context.scala
+++ src/main/scala/com/allaboutspark/chapter/one/tutorial_2/Context.scala
@@ -5,7 +5,7 @@ import org.apache.spark.sql.SparkSession
 
 trait Context {
 
-  lazy val sparkConf:SparkConf = sparkConf.setMaster("local[4]").setAppName("AllAboutSpark").set("spark.cores.max","2")
-  lazy val sparkSession:SparkSession = SparkSession.builder().config(sparkConf).getOrCreate()
+  lazy val sparkConf: SparkConf = new SparkConf().setMaster("local[4]").setAppName("AllAboutSpark").set("spark.cores.max","2")
+  lazy val sparkSession: SparkSession = SparkSession.builder().config(sparkConf).getOrCreate()
 
 }
--- src/main/scala/com/allaboutspark/chapter/one/tutorial_2/SparkSQL_Tutorial.scala
+++ src/main/scala/com/allaboutspark/chapter/one/tutorial_2/SparkSQL_Tutorial.scala
@@ -1,9 +1,18 @@
 package com.allaboutspark.chapter.one.tutorial_2
 
 object SparkSQL_Tutorial extends App with Context {
-  val dfTags=sparkSession.read
-    .option("inferSchema",true)
-    .option("header",true)
+
+  val dftags=sparkSession
+    .read
+    .option("header",value = true)
+    .option("inferSchema",value = true)
     .csv("D:\\Study_Materials\\Scala\\AllAboutSpark\\src\\main\\resources\\question_tags_10k.csv")
-    .toDF()
+    .toDF("Id","tag")
+  dftags.createOrReplaceTempView("temp_view")
+  //to validate whether the view has been registered with the spark session
+  sparkSession.catalog.listTables().show()
+  //using sql query to print all temp table created
+  sparkSession.sql("show tables").show(10)
+  //printing the columns using spark sql query
+  sparkSession.sql("select id,tag from temp_view limit 10").show()
 }
