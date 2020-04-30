80072adfc27059a6cb47278e43ab950a0a7e1143
ushapatturaj <ushadevipatturaj@gmail.com>
2020-04-30 13:27:55 +0200

    Learn Spark - Dataframe - creating subset of a dataframe, joining 2 dataframes, printing specific columns of a dataframes

--- src/main/scala/com/allaboutspark/chapter/one/tutorial_1/DataFrame_Tutorial_2.scala
+++ src/main/scala/com/allaboutspark/chapter/one/tutorial_1/DataFrame_Tutorial_2.scala
@@ -1,25 +1,41 @@
 package com.allaboutspark.chapter.one.tutorial_1
 
 object DataFrame_Tutorial_2 extends App with Context {
-  val dfTags=sparkSession
+  val dftags=sparkSession
+    .read
+    .option("header",value = true)
+    .option("inferSchema",value = true)
+    .csv("D:\\Study_Materials\\Scala\\AllAboutSpark\\src\\main\\resources\\question_tags_10k.csv")
+    .toDF("Id","tag")
+  val dfTagsQuestion=sparkSession
     .read
     .option("inferSchema",value=true)
     .option("header",value=true)
     .option("dateFormat","yyyy-MM-dd HH:mm:ss")
     .csv("D:\\Study_Materials\\Scala\\AllAboutSpark\\src\\main\\resources\\questions_10k.csv")
     .toDF("Id","CreationDate","ClosedDate","DeletionDate","Score","OwnerUserId","AnswerCount")
-  dfTags.show(10)
-  dfTags.printSchema()
-  val dftags_CSV=dfTags.select(
-    dfTags.col("Id").cast("integer"),
-    dfTags.col("CreationDate").cast("timestamp"),
-    dfTags.col("ClosedDate").cast("timestamp"),
-    dfTags.col("DeletionDate").cast("date"),
-    dfTags.col("Score").cast("integer"),
-    dfTags.col("OwnerUserId").cast("integer"),
-    dfTags.col("AnswerCount").cast("integer")
+  dfTagsQuestion.show(10)
+  dfTagsQuestion.printSchema()
+  val dftags_CSV=dfTagsQuestion.select(
+    dfTagsQuestion.col("Id").cast("integer"),
+    dfTagsQuestion.col("CreationDate").cast("timestamp"),
+    dfTagsQuestion.col("ClosedDate").cast("timestamp"),
+    dfTagsQuestion.col("DeletionDate").cast("date"),
+    dfTagsQuestion.col("Score").cast("integer"),
+    dfTagsQuestion.col("OwnerUserId").cast("integer"),
+    dfTagsQuestion.col("AnswerCount").cast("integer")
   )
   dftags_CSV.printSchema()
+  val scoreCount= dftags_CSV.filter("score>400 and score<=410").count()
+  val dftagCsvSubset=dftags_CSV.filter("score>400 and score<=410")
+  println(s"score count between 400 to 410 = $scoreCount")
+  dftagCsvSubset.show()
+  //Joining 2 dataframes
+  val joinedDF=dftagCsvSubset.join(dftags,"Id")
+  joinedDF.show()
+
+  //Print selected columns
+  joinedDF.select("Id","CreationDate","Score","AnswerCount","tag" ).show()
 
 
 }
