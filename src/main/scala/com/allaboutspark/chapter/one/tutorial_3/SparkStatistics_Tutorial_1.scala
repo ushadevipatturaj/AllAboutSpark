b179d02069ae305b7311962193d5ea19179a75cc
ushapatturaj <ushadevipatturaj@gmail.com>
2020-05-01 14:34:01 +0200

    Learn Spark - Spark Statistics - Avg, Min, Max, Mean, Sum

---
+++ src/main/scala/com/allaboutspark/chapter/one/tutorial_3/SparkStatistics_Tutorial_1.scala
@@ -0,0 +1,30 @@
+package com.allaboutspark.chapter.one.tutorial_3
+import org.apache.spark.sql.functions._
+object SparkStatistics_Tutorial_1 extends App with Context {
+  val dfTags = sparkSession.read
+    .option("inferSchema", value = true)
+    .option("header", value = true)
+    .csv("D:\\Study_Materials\\Scala\\AllAboutSpark\\src\\main\\resources\\question_tags_10k.csv")
+    .toDF("Id", "tag")
+
+  val dfTagQuestion = sparkSession.read
+    .option("inferSchema", value = true)
+    .option("header", value = true)
+    .option("dateFormat", "yyyy-MM-dd HH:mm:ss")
+    .csv("D:\\Study_Materials\\Scala\\AllAboutSpark\\src\\main\\resources\\questions_10k.csv")
+    .toDF("Id", "CreationDate", "ClosedDate", "DeletionDate", "Score", "OwnerUserId", "AnswerCount")
+
+  val dfTagQuestion_Formatted = dfTagQuestion.select(
+    dfTagQuestion.col("Id").cast("integer"),
+    dfTagQuestion.col("CreationDate").cast("timestamp"),
+    dfTagQuestion.col("ClosedDate").cast("timestamp"),
+    dfTagQuestion.col("DeletionDate").cast("date"),
+    dfTagQuestion.col("Score").cast("integer"),
+    dfTagQuestion.col("OwnerUserId").cast("integer"),
+    dfTagQuestion.col("AnswerCount").cast("integer")
+  )
+  dfTagQuestion_Formatted.select(avg("score")).show(10,truncate = false)
+  //dfTagQuestion_Formatted.select("Id").groupBy("Id").count().filter("count >1").show(10)
+  dfTagQuestion_Formatted.select( avg("score"),min("score"),max("score"),mean("score"),sum("score")).show()
+
+}
